c704e38b5766be646cee21bc4b896b6860023d3f
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 00:37:02 +0800

    Modify main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./pages/App";
+import "./styles/style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+const root = ReactDOM.createRoot(
+    document.querySelector("#root")
+);
+
+
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
