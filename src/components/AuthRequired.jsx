e724480b20e929cd387a2f81c12762f5ff52c39f
Lirae Que Data <liraedata59@gmail.com>
2023-08-16 15:30:16 +0800

    Add new AuthRequired component

---
+++ src/components/AuthRequired.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+
+const AuthRequired = () => {
+
+    const [token, setToken] = useState(null);
+    
+    useEffect(() => {
+        setToken(localStorage.getItem("token"));        
+    }, [token]);
+
+    return (
+        token === null ?    
+            <Navigate to="/signup"/> :
+            <Outlet />
+    );
+};
+
+export default AuthRequired;
