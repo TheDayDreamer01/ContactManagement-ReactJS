b310a2e76a513b1debab14fc7078b4cf623858b4
Lirae Que Data <liraedata59@gmail.com>
2023-08-16 15:29:19 +0800

    Add new constants.js file

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const AUTH_URL = "http://localhost:5230/api/auth";
+export const CONTACT_URL = "http://localhost:5230/api/contact";
+export const USER_URL = "http://localhost:5230/api/user";
+
