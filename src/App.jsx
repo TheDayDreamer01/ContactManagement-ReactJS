bc0503c08cde70a8d9b1a3398bf7a1e632af7472
Lirae Que Data <liraedata59@gmail.com>
2023-08-16 21:13:24 +0800

    Add framer-motion package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import SignIn from "./pages/SignIn"; 
-import SignUp from "./pages/SignUp";
+import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import AuthRequired from "./components/authRequired";
@@ -8,9 +7,7 @@ import AuthRequired from "./components/authRequired";
 const App = () => {
     return (
         <Routes>
-            <Route path="/" index element={ <SignUp />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/signin" element={<SignIn />} />
+            <Route path="/" index element={ <Auth />} />
         
             <Route element={<AuthRequired />}>
                 <Route path="/" element={<Dashboard />} />
