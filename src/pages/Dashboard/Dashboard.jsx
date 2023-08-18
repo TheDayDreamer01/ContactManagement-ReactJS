ff6633a48031e74179c2f49ee6605e0541f95c8b
Lirae Que Data <liraedata59@gmail.com>
2023-08-19 02:40:14 +0800

    Add context provider

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
-import { BiSun, BiMoon, BiSearch, BiMenuAltRight } from "react-icons/bi";
+import React, { useState, useEffect } from "react";
+import { BiSolidContact, BiLogOut, BiUser, BiX } from "react-icons/bi";
+import Header from "../../components/Header";
+
+export const Context = React.createContext();
 
 const Dashboard = () => {
-
+    
     const [isNavbar, setNavbar] = useState(false);
     const [isDark, setDark] = useState(
         localStorage.getItem("isDark") === "true"
@@ -13,46 +16,45 @@ const Dashboard = () => {
     }, [isDark]);
 
     return (
-        <div className={`${isDark ? "dark" : ""} flex flex-col h-screen w-screen`}>
-            <header className={`p-4 md:px-8 border-b border-zinc-900 dark:border-zinc-500 grid grid-cols-2 md:grid-cols-3 items-center ${isDark ? "dark:bg-zinc-800 dark:text-white" : ""}`}>
-                <div className="flex items-center">
-                    <button className="block md:hidden p-2 mr-2 rounded-md hover:bg-zinc-200"
-                            onClick={() => setNavbar(!isNavbar)}>
-                        <BiMenuAltRight size={30}/>
-                    </button>
-                    <h1 className="self-center text-3xl font-bold ">Nexio</h1>
-                </div>
-                <div className="hidden md:block relative">
-                    <input
-                        className="border pl-12 pr-2 border-zinc-900 dark:border-zinc-500 h-10 px-2 w-full justify-self-center bg-zinc-100 dark:bg-white dark:text-black rounded-sm"
-                        type="text" placeholder="Search Contact"
-                    />
-                    <BiSearch
-                        className="text-black absolute top-2 left-3"
-                        size={26}
-                    />
-                </div>
-                <button
-                    className="justify-self-end"
-                    onClick={() => setDark(!isDark)}
-                >
-                    {isDark ? <BiSun size={26} /> : <BiMoon size={26} />}
-                </button>
-            </header>
-            <main className="flex-grow flex">
-                <div className={`${isNavbar ? "block" : "hidden"} md:hidden h-full w-full bg-[#0000006f] absolute top-0 left-0 z-10`}
-                    onClick={() => setNavbar(!isNavbar)}>
-                </div>
+        <Context.Provider value={[ isNavbar, setNavbar, isDark, setDark ]}>
+            <div className={`${isDark ? "dark" : ""} flex flex-col h-screen w-screen`}>
+                <Header />
+                <main className="flex-grow flex">
+                    <div className={`${isNavbar ? "block" : "hidden"} md:hidden h-full w-full bg-[#0000006f] absolute top-0 left-0 z-10`}
+                        onClick={() => setNavbar(!isNavbar)}>
+                    </div>
 
-                <nav className={`${isNavbar ? "translate-x-0" : "translate-x-[-100%]"} md:translate-x-0 bg-white w-72 absolute top-0 z-20 h-screen md:relative md:h-full transition-all ease-linear border-r border-zinc-900 dark:border-zinc-500 dark:bg-zinc-800`}>
-                    hi
-                </nav>
+                    <nav className={`${isNavbar ? "translate-x-0" : "translate-x-[-100%]"} md:translate-x-0 bg-white w-72 absolute top-0 z-20 h-screen md:relative md:h-full transition-all ease border-r border-zinc-900 dark:border-zinc-500 dark:bg-zinc-800`}>
+                        <div className="flex flex-col h-full p-4">
+                            <button className="md:hidden self-end p-2 mb-4 hover:bg-zinc-200 rounded-lg" 
+                                    onClick={() => setNavbar(!isNavbar)}>
+
+                                <BiX size={40}/>
+                            </button>
+                            <SideBarItem icon={<BiSolidContact size={24} />} title="Contacts" />
+                            <SideBarItem icon={<BiUser size={24} />} title="Profile" />
+                            <div className="flex-grow"></div>
+                            <SideBarItem icon={<BiLogOut size={24} />} title="Sign Out" />
+                        </div>
+                    </nav>
+
+                    <div className="flex-grow bg-zinc-100 dark:bg-zinc-700">
+                    </div>
+                </main>
+            </div>
+        </Context.Provider>
         
-                <div className="flex-grow bg-zinc-100 dark:bg-zinc-700">
-                </div>
-            </main>
-        </div>
     );
 };
 
+
+// eslint-disable-next-line react/prop-types
+const SideBarItem = ({ icon, title }) => {
+    return (
+        <button className="flex items-center gap-4 p-4 rounded-md bg-zinc-100 hover:bg-zinc-200 mb-2">
+            {icon}
+            <p className="text-md leading-2 font-normal">{title}</p>
+        </button>
+    );
+};
 export default Dashboard;
