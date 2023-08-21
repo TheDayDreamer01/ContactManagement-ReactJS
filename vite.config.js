3fd28bb21b90b59445534b5579f53d30b74fc9e2
Lirae Que Data <liraedata59@gmail.com>
2023-08-21 21:34:16 +0800

    Configure backend connection

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server : {
-    port : 3000,
+    port : 5230,
     proxy : {
       "/api" : {
-        target : "http://localhost:8000/",
+        target : "http://localhost:5230/",
         changeOrigin : true
       }
     }
