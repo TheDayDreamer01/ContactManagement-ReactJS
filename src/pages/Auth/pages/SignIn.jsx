f16eb72d4694374c4f7070fb2aff50b9867e0355
Lirae Que Data <liraedata59@gmail.com>
2023-08-22 01:15:42 +0800

    Modify form validation

--- src/pages/Auth/pages/SignIn.jsx
+++ src/pages/Auth/pages/SignIn.jsx
@@ -1,27 +1,120 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const SignIn = () => {
-    return (
-        <motion.div initial={{opacity : 0}} animate={{opacity : 1}}>
-            <section className="flex flex-col space-y-4">
-                <div className="text-center">
-                    <h1 className="text-3xl font-bold py-4 md:text-5xl">Sign In</h1>
-                    <p className="text-sm md:m-2">Welcome back! Please sign in to access your account.</p>
-                </div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [formError, setFormError] = useState({});
+  const [visible, setVisible] = useState(false);
 
-                <form className="flex flex-col">    
-                    <label className="ml-2 py-2 text-sm" htmlFor="email">Email</label>
-                    <input className="drop-shadow-md h-10 mb-3 px-2 rounded-md dark:bg-neutral-800" id="email" type="text" />
-                    <label className="ml-2 py-2 text-sm" htmlFor="password">Password</label>
-                    <input className="drop-shadow-md h-10 mb-3 px-2 rounded-md dark:bg-neutral-800" id="password" type="password" />
+  const setFormValue = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-                    <Link className="mb-10 self-end text-sm underline" path="/">Forgot Password?</Link>
-                    <button className="h-12 bg-neutral bg-neutral-900 text-white text-sm rounded-md dark:bg-neutral-950">Sign In</button>
-                </form>
-            </section>
-        </motion.div>
-    );
+  const showPassword = (e) => {
+    e.preventDefault();
+    setVisible(!visible);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm(formData);
+    if (Object.keys(errors).length === 0) {
+      alert("Yehey");
+    } else {
+      setFormError(errors);
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!data.email.trim()) {
+      errors.email = "Email is required.";
+    } else if (!data.email.match(emailPattern)) {
+      errors.email = "Invalid email address.";
+    }
+
+    if (data.password.trim().length < 6) {
+      errors.password = "Password must at least be 6 characters long.";
+    }
+
+    return errors;
+  };
+
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <section className="flex flex-col space-y-4">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold py-4 md:text-5xl">Sign In</h1>
+          <p className="text-sm md:m-2">
+            Welcome back! Please sign in to access your account.
+          </p>
+        </div>
+
+        <form className="flex flex-col" onSubmit={onFormSubmit}>
+          <label
+            className="mt-2 py-2 text-sm text-neutral-600 dark:text-neutral-200"
+            htmlFor="email"
+          >
+            Email:
+          </label>
+          <input
+            className={`${
+              formError.email && "border border-red-500"
+            } drop-shadow-md h-10 px-2 rounded-md dark:bg-neutral-700`}
+            id="email"
+            type="text"
+            name="email"
+            onChange={setFormValue}
+          />
+          {formError.email && (
+            <p className="text-sm text-red-600">{formError.email}</p>
+          )}
+
+          <label
+            className="mt-2 py-2 text-sm text-neutral-600 dark:text-neutral-200"
+            htmlFor="password"
+          >
+            Password:
+          </label>
+          <div className="relative">
+            <input
+              className={`${
+                formError.password && "border border-red-500"
+              } w-full drop-shadow-md h-10 pl-2 pr-14 rounded-md dark:bg-neutral-700`}
+              id="password"
+              type={visible ? "text" : "password"}
+              name="password"
+              onChange={setFormValue}
+            />
+            <button className="absolute right-4 top-2" onClick={showPassword}>
+              {visible ? <FiEyeOff size={24} /> : <FiEye size={24} />}
+            </button>
+          </div>
+          {formError.password && (
+            <p className="text-sm text-red-600">{formError.password}</p>
+          )}
+
+          <Link className="mt-4 mb-8 self-end text-sm underline" path="/">
+            Forgot Password?
+          </Link>
+          <button className="h-12 bg-neutral-800 text-white text-sm rounded-md dark:bg-neutral-900">
+            Sign In
+          </button>
+        </form>
+      </section>
+    </motion.div>
+  );
 };
 
 export default SignIn;
