456efa87d6c2cc28400d8050e84aaa73671328a4
Lirae Que Data <liraedata59@gmail.com>
2023-08-22 01:52:50 +0800

    Modify api return value

--- src/services/authService.js
+++ src/services/authService.js
@@ -12,19 +12,17 @@ const instance = axios.create({
 export const SignUpService = async (userData) => {
     try {
         const response = await instance.post("/signup", userData);
-        return response.data;
+        return response;
     } catch (error) {
-        console.log(error);
-        throw new Error("Failed to sign up. Please try again later.");
+        return error.response;
     }
 };
 
 export const SignInService = async (userData) => {
     try {
         const response = await instance.post("/signin", userData);
-        return response.data;
+        return response;
     } catch (error) {
-        console.log(error);
-        throw new Error("Failed to sign in. Please check your credentials and try again.");
+        return error.response;
     }
 };
