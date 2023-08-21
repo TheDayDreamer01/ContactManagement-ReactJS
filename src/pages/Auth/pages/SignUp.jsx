89e21419c36d1a7540e45a367132cde91600dbf8
Lirae Que Data <liraedata59@gmail.com>
2023-08-21 21:22:41 +0800

    Modify dark mode in SignUp.jsx

--- src/pages/Auth/pages/SignUp.jsx
+++ src/pages/Auth/pages/SignUp.jsx
@@ -13,23 +13,23 @@ const SignUp = () => {
                     <div className="flex gap-4 mb-3 md:gap-6">
                         <span className="flex-grow">
                             <label className="ml-2 py-2 text-sm block" htmlFor="first-name">First Name</label>
-                            <input className="drop-shadow-md px-2 h-10 w-full rounded-md dark:bg-neutral-600" id="first-name" type="text" />
+                            <input className="drop-shadow-md px-2 h-10 w-full rounded-md dark:bg-neutral-800" id="first-name" type="text" />
                         </span>
                         <span className="flex-grow">
                             <label className="ml-2 py-2 text-sm block" htmlFor="last-name">Last Name</label>
-                            <input className="drop-shadow-md px-2 h-10 w-full rounded-md dark:bg-neutral-600" id="last-name" type="text" />
+                            <input className="drop-shadow-md px-2 h-10 w-full rounded-md dark:bg-neutral-800" id="last-name" type="text" />
                         </span>
                     </div>
                     <label className="ml-2 py-2 text-sm" htmlFor="username">Username</label>
-                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-600" id="username" type="text" />
+                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-800" id="username" type="text" />
                     <label className="ml-2 py-2 text-sm" htmlFor="email">Email</label>
-                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-600" id="email" type="text" />
+                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-800" id="email" type="text" />
                     <label className="ml-2 py-2 text-sm" htmlFor="password">Password</label>
-                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-600" id="password" type="password" />
+                    <input className="drop-shadow-md px-2 h-10 mb-3 rounded-md dark:bg-neutral-800" id="password" type="password" />
                     <label className="ml-2 py-2 text-sm" htmlFor="confirm-password">Confirm Password</label>
-                    <input className="drop-shadow-md px-2 h-10 mb-10 rounded-md dark:bg-neutral-600" id="confirm-password" type="password" />
+                    <input className="drop-shadow-md px-2 h-10 mb-10 rounded-md dark:bg-neutral-800" id="confirm-password" type="password" />
 
-                    <button className="h-12 bg-neutral-900 text-white text-sm rounded-md">Sign Up</button>
+                    <button className="h-12 bg-neutral-950 text-white text-sm rounded-md">Sign Up</button>
                 </form>                
             </section>
         </motion.div>
