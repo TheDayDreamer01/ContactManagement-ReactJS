806baedcae0c3eab05b64888956403a8852998c7
Lirae Que Data <liraedata59@gmail.com>
2023-08-22 23:17:36 +0800

    Modify Button Colors for darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,37 @@ import { BiSearch, BiMenu, BiMoon, BiSun } from "react-icons/bi";
 import { Context } from "../pages/Dashboard";
 
 const Header = () => {
+  const [navBar, setNavBar, dark, setDark] = useContext(Context);
 
-    const [ navBar, setNavBar, dark, setDark ] = useContext(Context);
-    
-    return (
-        <header className="p-4 dark:bg-neutral-900 transition-colors ease">
-            <div className="md:mx-4 flex justify-between items-center">
-                <button className="block md:hidden p-2 shadow-md rounded-full dark:bg-neutral-800 transition-colors ease dark:text-neutral-400 mr-2 bg-white"
-                        onClick={() => setNavBar(!navBar)}>
-                    <BiMenu size={26} />
-                </button>
-                <div className="relative w-full md:w-96">
-                    <input  className="h-10 w-full rounded-full pl-11 pr-2 drop-shadow-md dark:bg-neutral-800 transition-colors ease dark:text-white"
-                            type="text" 
-                            placeholder="Search contact"/>
-                    <BiSearch className=" absolute top-2 left-3 text-neutral-400"
-                            size={26} />
-                </div>
-                <button className="hidden md:block p-2 shadow-md rounded-full dark:bg-neutral-800 transition-colors ease dark:text-neutral-400 bg-white"
-                        onClick={() => setDark(!dark)}>
-                    {(dark) ? <BiSun size={24} /> : <BiMoon size={24} /> } 
-                </button>
-            </div>
-        </header>
-    );
+  return (
+    <header className="p-4 transition-colors ease dark:bg-neutral-800">
+      <div className="md:mx-4 flex justify-between items-center">
+        <button
+          className="block p-2 mr-2 bg-white shadow-md rounded-full transition-colors ease dark:bg-neutral-700 md:hidden dark:text-neutral-400"
+          onClick={() => setNavBar(!navBar)}
+        >
+          <BiMenu size={26} />
+        </button>
+        <div className="relative w-full md:w-96">
+          <input
+            className="h-10 w-full rounded-full pl-11 pr-2 drop-shadow-md transition-colors ease dark:bg-neutral-700 dark:text-white"
+            type="text"
+            placeholder="Search contact"
+          />
+          <BiSearch
+            className=" absolute top-2 left-3 text-neutral-600"
+            size={26}
+          />
+        </div>
+        <button
+          className="hidden p-2 shadow-md rounded-full transition-colors ease dark:text-neutral-400 bg-white md:block dark:bg-neutral-900"
+          onClick={() => setDark(!dark)}
+        >
+          {dark ? <BiSun size={24} /> : <BiMoon size={24} />}
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
