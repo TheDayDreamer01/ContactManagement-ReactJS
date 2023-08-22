2425a350bf586feb29848455510fff02e0191d0a
Lirae Que Data <liraedata59@gmail.com>
2023-08-22 23:17:15 +0800

    Integrate ContactList page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { ContactItem } from "../../components/ContactItems";
 import { GetUserContacts } from "../../services/contactService.js";
-
+import World from "../../assets/svg/World.svg";
 
 const Contact = ({ onSelectedContact }) => {
   const [contactData, setContactData] = useState([]);
@@ -20,17 +20,32 @@ const Contact = ({ onSelectedContact }) => {
 
   return (
     <>
-      {contactData.map((element, index) => (
-        <ContactItem
-          key={index}
-          firstName={element.firstName}
-          lastName={element.lastName}
-          phone={element.phoneNo}
-          email={element.email}
-          date="10/10/2023"
-          onContactView={() => onSelectedContact(element.id)}
-        />
-      ))}
+      {contactData ? (
+        <>
+          {contactData.map((element, index) => (
+            <ContactItem
+              key={index}
+              firstName={element.firstName}
+              lastName={element.lastName}
+              phone={element.phoneNo}
+              email={element.email}
+              date="10/10/2023"
+              onContactView={() => onSelectedContact(element.id)}
+            />
+          ))}
+        </>
+      ) : (
+        <div className="h-full flex flex-col justify-center items-center p-4 gap-8">
+          <img
+            className="max-w-sm md:max-w-lg mx-auto"
+            src={World}
+            alt="No Available Contacts"
+          />
+          <h1 className="text-lg md:text-2xl font-semibold text-center dark:text-white">
+            No Available Contacts
+          </h1>
+        </div>
+      )}
     </>
   );
 };
