f43ab0491c2687c953b69efec03c31a98a2586db
Lirae Que Data <liraedata59@gmail.com>
2023-08-22 23:17:59 +0800

    Modify Colors for darkmode

--- src/components/ModalBox.jsx
+++ src/components/ModalBox.jsx
@@ -1,37 +1,54 @@
+/* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
 
-/* eslint-disable react/prop-types */
-const ModalBox = ({icon, title, message, onAccept, onCancelValue, onCancel}) => {
-    return (
-        <motion.section initial={{opacity : 0}}
-                        animate={{opacity : (onCancelValue) ? 1 : 0 }}
-                        className="absolute bg-[#0000006f] h-screen w-screen z-40" 
-                        onClick={onCancel}>
-            <div className="flex justify-center items-center">
-                <motion.div initial={{translateY : -100}}
-                            animate={{translateY: 0 }}
-                            className="bg-white h-80 md:h-80 w-80 md:w-[30rem] relative top-28 rounded-lg shadow-md">
-                    <div className="p-8 flex flex-col items-center justify-center h-full">
-                        <div className="flex-grow flex flex-col items-center justify-center gap-4 text-center mb-8">
-                            {icon} 
-                            <h1 className="text-2xl font-medium">{title}</h1>    
-                            <p className="text-sm text-neutral-500 max-w-[14rem]">{message}</p>
-                        </div>
-                        <div className="flex gap-4">
-                            <button className="bg-red-500 border border-red-500 text-sm text-white px-5 py-3 rounded-lg shadow-md"
-                                    onClick={onAccept}>
-                                {"Yes, I'm sure"}
-                            </button>
-                            <button className="border shadow-md border-black text-sm text-black px-5 py-3 rounded-lg"
-                                    onClick={onCancel}>
-                                {"No, Cancel"}
-                            </button>
-                        </div>
-                    </div>
-                </motion.div>
+const ModalBox = ({
+  icon,
+  title,
+  message,
+  onAccept,
+  onCancelValue,
+  onCancel,
+}) => {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: onCancelValue ? 1 : 0 }}
+      className="absolute bg-[#0000006f] h-screen w-screen z-40"
+      onClick={onCancel}
+    >
+      <div className="flex justify-center items-center">
+        <motion.div
+          initial={{ translateY: -100 }}
+          animate={{ translateY: 0 }}
+          className="bg-white h-80 md:h-80 w-80 md:w-[30rem] relative top-28 rounded-lg shadow-md dark:bg-neutral-700 dark:text-white"
+        >
+          <div className="p-8 flex flex-col items-center justify-center h-full">
+            <div className="flex-grow flex flex-col items-center justify-center gap-4 text-center mb-8">
+              {icon}
+              <h1 className="text-2xl font-medium">{title}</h1>
+              <p className="text-sm text-neutral-600 max-w-[14rem] dark:text-neutral-200">
+                {message}
+              </p>
             </div>
-        </motion.section>
-    );
+            <div className="flex gap-4">
+              <button
+                className="bg-red-500 border border-red-500 text-sm text-white px-5 py-3 rounded-lg shadow-md"
+                onClick={onAccept}
+              >
+                {"Yes, I'm sure"}
+              </button>
+              <button
+                className="border shadow-md border-black text-sm text-black px-5 py-3 rounded-lg dark:text-white dark:border-white"
+                onClick={onCancel}
+              >
+                {"No, Cancel"}
+              </button>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default ModalBox;
