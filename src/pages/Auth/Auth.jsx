a6fee10fb651710d6f82766777c7ea4b64eac554
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 06:21:26 +0800

    Add Loading Screen upon onClick

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -3,22 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { TbHexagon3D } from "react-icons/tb";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Loading from "../../components/Loading";
 
 const Auth = () => {
   const navigate = useNavigate();
   const [register, setRegister] = useState(false);
   const [isDark, setDark] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setDark(localStorage.getItem("isDark") === "true");
     const isAuthenticated = sessionStorage.getItem("token");
     if (isAuthenticated !== null) {
       navigate("/dashboard", { replace: true });
-    } 
+    }
   }, [setDark, navigate]);
+
+  const onSetLoading = () => setLoading(!loading);
 
   return (
     <div className={`${isDark ? "dark" : ""}`}>
+
+      {loading && <Loading />}
       <div className="flex flex-col h-screen p-4 md:px-8 items-center dark:bg-neutral-800 dark:text-white">
         <div className="self-start flex items-center">
           <TbHexagon3D size={40} className="mr-1" />
@@ -44,7 +50,11 @@ const Auth = () => {
             </button>
           </nav>
 
-          {register ? <SignUp /> : <SignIn />}
+          {register ? (
+            <SignUp onSetLoading={onSetLoading} />
+          ) : (
+            <SignIn onSetLoading={onSetLoading} />
+          )}
         </div>
 
         <div className="flex-grow"></div>
