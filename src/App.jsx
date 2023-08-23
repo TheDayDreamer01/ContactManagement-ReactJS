8409542ff81ceea322baffa38ade9905d46445f4
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 04:18:47 +0800

    Add new internal server  route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound/NotFound";
+import Errors from "./pages/Errors";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Route path="/" index element={ <Auth />} />
             <Route path="/*" element={<NotFound />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/error" element={<Errors />} />
         </Routes>
     );
 };
