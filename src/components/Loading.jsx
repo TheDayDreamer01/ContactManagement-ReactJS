b8b569e4c999d60c812b23fcbb65c99e7323b87a
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 04:15:54 +0800

    Add new Loading page

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+    return (
+        <div className="absolute top-0 left-0 h-screen w-screen z-10 bg-neutra bg-[#000000de]">
+            <div className="flex justify-center items-center h-full">
+                <div className="loader ease-linear rounded-full border-8 border-t-8 border-white h-32 w-32">
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
