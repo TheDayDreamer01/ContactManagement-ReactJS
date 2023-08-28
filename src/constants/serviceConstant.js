fdfa3384f8e1b0728cf908ae28568d24527753f6
Lirae Que Data <liraedata59@gmail.com>
2023-08-29 00:11:41 +0800

    Add new production routes

--- src/constants/serviceConstant.js
+++ src/constants/serviceConstant.js
@@ -1,4 +1,11 @@
-export const AUTH_URL = "http://localhost:5230/api/auth";
-export const CONTACT_URL = "http://localhost:5230/api/contact";
-export const USER_URL = "http://localhost:5230/api/user";
+// Ports for Production Environment
+export const AUTH_URL = "http://localhost:8000/api/auth";
+export const CONTACT_URL = "http://localhost:8000/api/contact";
+export const USER_URL = "http://localhost:8000/api/user";
+
+// Ports for Development Environment
+export const DEV_AUTH_URL = "http://localhost:5230/api/auth";
+export const DEV_CONTACT_URL = "http://localhost:5230/api/contact";
+export const DEV_USER_URL = "http://localhost:5230/api/user";
+
 
