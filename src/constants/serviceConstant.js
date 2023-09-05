2228a4434209bace84d82c0d5cab109e6acb7282
Lirae Que Data <liraedata59@gmail.com>
2023-09-05 22:09:05 +0800

    Remove vite env variable

--- src/constants/serviceConstant.js
+++ src/constants/serviceConstant.js
@@ -1,9 +1,4 @@
 /* eslint-disable no-undef */
-// Ports for Production Environment
-export const AUTH_URL = import.meta.env.VITE_AUTH_URL;
-export const CONTACT_URL = import.meta.env.VITE_CONTACT_URL;
-export const USER_URL = import.meta.env.VITE_USER_URL;
-
-// export const AUTH_URL="http://localhost:8000/api/auth";
-// export const CONTACT_URL="http://localhost:8000/api/contact";
-// export const USER_URL="http://localhost:8000/api/user";
+export const AUTH_URL="http://localhost:8000/api/auth";
+export const CONTACT_URL="http://localhost:8000/api/contact";
+export const USER_URL="http://localhost:8000/api/user";
