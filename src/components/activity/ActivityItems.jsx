7f1ef8ca43da68965a77291e61ee8649fc35ab47
Lirae Que Data <liraedata59@gmail.com>
2023-09-05 22:08:37 +0800

    Add new activity header component

--- src/components/activity/ActivityItems.jsx
+++ src/components/activity/ActivityItems.jsx
@@ -32,14 +32,22 @@ export const ActivityItem = ({ status, description, date }) => {
  *
  * This component displays the header of the activity list, including column titles.
  */
-export const ActivityHeader = () => {
+export const ActivityTitleHeader = () => {
   return (
-    <div className="p-4 w-full text-start rounded-md my-1 bg-neutral-800 text-white font-semibold dark:bg-neutral-900 ">
+    <div className="p-4 w-full text-start rounded-md my-1 bg-neutral-200  font-semibold dark:bg-neutral-800 dark:text-white">
       <div className="grid grid-cols-3 xl:grid-cols-6">
         <h1 className="hidden xl:block">Status</h1>
         <p className="col-span-2 max-w-[38rem] xl:col-span-4">Description</p>
         <p>Date</p>
       </div>
+    </div>
+  );
+};
+
+export const ActivityHeader = () => {
+  return (
+    <div className="p-4 w-full text-start rounded-md my-1 bg-neutral-800 text-white dark:bg-neutral-900 ">
+      <h1 className="text-start text-xl font-semibold">Recent Activities</h1>
     </div>
   );
 };
